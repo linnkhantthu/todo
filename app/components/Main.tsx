af04add686c8119e3a4c8904f8061d81596cca2c
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2023-10-29 15:32:59 +0630

    ui modification on navbar

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 function Main({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="container flex flex-row w-11/12 mb-10 overflow-x-hidden">
+      <main className="container flex flex-row w-11/12 mt-16 mb-10 overflow-x-hidden">
         {children}
       </main>
     </>
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,7 +4,7 @@ import UserNavbar from "./UserNavbar";
 
 function Nav({ isLoggedIn }: { isLoggedIn: boolean }) {
   return (
-    <nav className="navbar bg-base-100">
+    <nav className="navbar bg-base-100 p-2 mt-0 fixed w-full z-10 top-0">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">Todo</a>
       </div>
